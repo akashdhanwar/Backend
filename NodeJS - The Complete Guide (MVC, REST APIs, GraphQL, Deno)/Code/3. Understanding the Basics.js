7b04c7c6c87380384d0846c42a72c557d47f0843
akashdhanwar <43340559+akashdhanwar@users.noreply.github.com>
2024-03-01 12:33:15 +0530

    Update 3. Understanding the Basics.js

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Code/3. Understanding the Basics.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Code/3. Understanding the Basics.js
@@ -7,10 +7,19 @@ const server = http.createServer((req,res)=>{
 // console.log(req);
 // # 2. Reading request
   console.log(req.url, req.method, req.headers);
+// # 4. Routing Request
+  const url = req.url;
+  if(url=== '/'){
+     res.write('<html>')
+     res.write('<head><title>Enter Message</title></head>');
+     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>);
+     res.write('</html>);
+     return res.end();
+  }
 // # 3. Sending back response 
-  res.setHeader('Content-Type','text/html');
+// res.setHeader('Content-Type','text/html');
   res.write('<html>')
-  res.write('<head><title>My First Page</title></head>');
+  res.write('<head><title>My First Page</title></heady>');
   res.write('<body><h1>Hello from my Node.js Server</h1></body>);
   res.write('</html>);
   res.end();
