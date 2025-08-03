8c9299ed0d578e56500b3ec069f05f750c16980b
Akash Dhanwar <akash.dhanwar112@gmail.com>
2025-08-03 13:08:54 +0530

    Updates

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Code/3. Understanding the Basics.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Code/3. Understanding the Basics.js
@@ -1,28 +1,37 @@
 // # 1. Creating a Node.js Server
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 const PORT_NUMBER = 3000;
-// function rqListener(req, res) {
-// }
-const server = http.createServer((req,res)=>{
-// console.log(req);
-// # 2. Reading request
+
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  // # 2. Reading request
   console.log(req.url, req.method, req.headers);
-// # 4. Routing Request
+  // # 4. Routing Request
   const url = req.url;
-  if(url=== '/'){
-     res.write('<html>')
-     res.write('<head><title>Enter Message</title></head>');
-     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-     res.write('</html>');
-     return res.end();
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end(); // We return so that we must not execute further lines
   }
-// # 3. Sending back response 
-// res.setHeader('Content-Type','text/html');
-  res.write('<html>')
-  res.write('<head><title>My First Page</title></heady>');
-  res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-  res.write('</html>');
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  // # 3. Sending back response
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></heady>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
   res.end();
-  // res.write() now will not throw error here but we should not write like this 
+  // res.write() now will not throw error here but we should not write like this
 });
 server.listen(PORT_NUMBER);
