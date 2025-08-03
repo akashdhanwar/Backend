26e865581ce7a5137b21c2d9b5f1cf453bdd349f
Akash Dhanwar <akash.dhanwar112@gmail.com>
2025-08-03 15:22:36 +0530

    Updates

---
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Code/4. routes.js
@@ -0,0 +1,7 @@
+// # 1. Creating a Node.js Server
+const http = require("http");
+const routes = require("./3. routes");
+const PORT_NUMBER = 3000;
+
+const server = http.createServer(routes);
+server.listen(PORT_NUMBER);
